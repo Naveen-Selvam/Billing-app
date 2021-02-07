4b09b8e75913ede17ab0a33f7fcbaecc93050dc6
naveens <joinnaveens@gmail.com>
2021-02-07 10:14:29 +0530

    updated with download invoice

--- src/components/bill/Inovice.js
+++ src/components/bill/Inovice.js
@@ -10,6 +10,8 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const Inovice = (props) => {
   const dispatch = useDispatch();
@@ -50,47 +52,62 @@ const Inovice = (props) => {
   return (
     <div>
       <Modal isOpen={billId !== "undefined"}>
-        <h1>Invoice</h1>
-        <h2>Date : {currentBill?.date?.slice(0, 10)}</h2>
-        <h2>Customer Name : {customer[0]?.name}</h2>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow style={{ backgroundColor: "#404040" }}>
-                <TableCell>
-                  <h4>Product</h4>
-                </TableCell>
-                <TableCell>
-                  <h4>Price</h4>
-                </TableCell>
-                <TableCell>
-                  <h4>qty</h4>
-                </TableCell>
-                <TableCell>
-                  <h4>subtotal</h4>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {product.map((pdt, i) => {
-                return (
-                  <TableRow key={pdt._id}>
-                    <TableCell>{pdt.name}</TableCell>
-                    <TableCell>{pdt.price}</TableCell>
-                    <TableCell>{currentBill?.lineItems[i].quantity}</TableCell>
-                    <TableCell>{currentBill?.lineItems[i].subTotal}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <h2> Total Amount = {currentBill.total}</h2>
-        <div style={{ textAlign: "center" }}>
-          <Button color="secondary" onClick={handleClose}>
-            close
-          </Button>
+        <div ref={ref}>
+          <h1>Invoice</h1>
+          <h2>Date : {currentBill?.date?.slice(0, 10)}</h2>
+          <h2>Customer Name : {customer[0]?.name}</h2>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow style={{ backgroundColor: "#404040" }}>
+                  <TableCell>
+                    <h4>Product</h4>
+                  </TableCell>
+                  <TableCell>
+                    <h4>Price</h4>
+                  </TableCell>
+                  <TableCell>
+                    <h4>qty</h4>
+                  </TableCell>
+                  <TableCell>
+                    <h4>subtotal</h4>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {product.map((pdt, i) => {
+                  return (
+                    <TableRow key={pdt._id}>
+                      <TableCell>{pdt.name}</TableCell>
+                      <TableCell>{pdt.price}</TableCell>
+                      <TableCell>
+                        {currentBill?.lineItems[i].quantity}
+                      </TableCell>
+                      <TableCell>
+                        {currentBill?.lineItems[i].subTotal}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <h2> Total Amount = {currentBill.total}</h2>
+          <div style={{ textAlign: "center" }}>
+            <Button color="secondary" onClick={handleClose}>
+              close
+            </Button>
+          </div>
         </div>
+        <Pdf targetRef={ref} filename="code-example.pdf">
+          {({ toPdf }) => (
+            <div style={{ textAlign: "center" }}>
+              <Button color="primary" onClick={toPdf}>
+                Download Bill
+              </Button>
+            </div>
+          )}
+        </Pdf>
       </Modal>
     </div>
   );
